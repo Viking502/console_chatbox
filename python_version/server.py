97321ca12e647ca74a7856a5c0b29a6fbed0a4be
Viking <wiktor.przadka.1@gmail.com>
2020-08-06 00:43:28 +0200

    use json to encode data before sending

--- python_version/server.py
+++ python_version/server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 from datetime import datetime
+import json
 
 
 class Server:
@@ -13,23 +14,25 @@ class Server:
 
         self.hosts = []
 
-    def print_msg(self, addr, msg):
-        print(f'\033[1;33m{addr[0]}:{addr[1]}: {datetime.now().strftime("%H:%M:%S %d-%m-%y")}\n{msg}\033[0m')
+    @staticmethod
+    def print_msg(message):
+        print(f'\033[1;33m{message["auth"]}: {message["time"]}\n{message["msg"]}\033[0m')
 
     def read_handler(self, conn, addr):
         while True:
             buff = conn.recv(1024)
             if buff:
-                self.print_msg(addr, buff.decode("utf-8"))
+                jsoned_msg = json.dumps({'auth': addr, 'msg': buff.decode('utf-8'), 'time': datetime.now().strftime("%H:%M:%S %d-%m-%y")})
+                self.print_msg(json.loads(jsoned_msg))
                 for host in self.hosts:
-                    if host['conn'] != conn:
-                        host['conn'].send(buff)
+                    host['conn'].send(bytes(jsoned_msg, 'utf-8'))
 
     def run(self):
         while True:
             conn, host_addr = self.sock.accept()
+            host_addr = f'{host_addr[0]}:{host_addr[1]}'
             self.hosts.append({'addr': host_addr, 'conn': conn})
-            print(f'({host_addr[0]}:{host_addr[1]}) has connected')
+            print(f'({host_addr}) has connected')
 
             reader = threading.Thread(target=self.read_handler, args=(conn, host_addr), daemon=True)
             reader.start()
