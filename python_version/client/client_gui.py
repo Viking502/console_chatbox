0d4094fa1d058677017620b1722d8ce3d18a51a0
Viking <wiktor.przadka.1@gmail.com>
2020-08-26 23:47:51 +0200

    add login layer

--- python_version/client/client_gui.py
+++ python_version/client/client_gui.py
@@ -4,15 +4,11 @@ import sys
 import threading
 from python_version.client.client_core import ClientCore
 
+class MessagesLayout:
 
-class ChatWidget(QtW.QWidget):
+    def __init__(self, high, core):
+        self.core = core
 
-    msg_signal = Signal(dict)
-
-    def __init__(self, config: dict):
-        QtW.QWidget.__init__(self)
-
-        # initialize gui
         self.layout = QtW.QGridLayout()
 
         self.scroll = QtW.QScrollArea()
@@ -28,22 +24,16 @@ class ChatWidget(QtW.QWidget):
         self.wrapper.setLayout(self.messages_box)
 
         self.send_box = QtW.QTextEdit("")
-        self.send_box.setFixedHeight(self.height() // 8)
+        self.send_box.setFixedHeight(high // 8)
         self.layout.addWidget(self.send_box, 3, 0)
 
         self.send_button = QtW.QPushButton("send!")
         self.send_button.setStyleSheet("QPushButton:hover { background-color: rgb(200, 200, 200) }")
         self.layout.addWidget(self.send_button, 3, 1)
-
-        self.setLayout(self.layout)
-
-        # initialize backend
-        self.core = ClientCore(config)
-        self.core.connect()
-
-        reader = threading.Thread(target=self.read_handler, daemon=True)
-        reader.start()
         self.send_button.clicked.connect(self.send_msg)
+
+    def get(self):
+        return self.layout
 
     def send_msg(self):
         msg = self.send_box.toPlainText()
@@ -63,6 +53,65 @@ class ChatWidget(QtW.QWidget):
         message.setStyleSheet("QLabel { border: 2px solid rgb(200, 200, 200)}")
         self.messages_box.addWidget(message)
         self.scroll.verticalScrollBar().setValue(self.scroll.verticalScrollBar().maximum())
+
+
+class LoginLayout:
+
+    def __init__(self, core):
+        self.core = core
+
+        self.layout = QtW.QGridLayout()
+
+        self.login_label = QtW.QLabel('nick: ')
+        self.login_label.setFixedHeight(60)
+        self.login_label.setFixedWidth(280)
+        self.layout.addWidget(self.login_label, 0, 0)
+        self.login_box = QtW.QTextEdit()
+        self.login_box.setFixedHeight(60)
+        self.login_box.setFixedWidth(280)
+        self.layout.addWidget(self.login_box, 0, 1)
+
+        self.pass_label = QtW.QLabel('password: ')
+        self.layout.addWidget(self.pass_label, 1, 0)
+        self.pass_box = QtW.QTextEdit()
+        self.pass_box.setFixedHeight(60)
+        self.pass_box.setFixedWidth(280)
+        self.layout.addWidget(self.pass_box, 1, 1)
+
+        self.login_button = QtW.QPushButton("log_in")
+        self.login_button.setStyleSheet("QPushButton:hover { background-color: rgb(200, 200, 200) }")
+        self.login_button.clicked.connect(self.log_in)
+        self.layout.addWidget(self.login_button, 2, 1)
+
+    def log_in(self):
+        nick = self.login_box.toPlainText()
+        password = self.pass_box.toPlainText()
+        print(nick, password)
+        self.core.log_in(nick=nick, password=password)
+
+    def get(self):
+        return self.layout
+
+
+class ChatWidget(QtW.QWidget):
+
+    msg_signal = Signal(dict)
+
+    def __init__(self, config: dict):
+        QtW.QWidget.__init__(self)
+
+        # initialize backend
+        self.core = ClientCore(config)
+        self.core.connect()
+
+        reader = threading.Thread(target=self.read_handler, daemon=True)
+        reader.start()
+
+        # initialize gui
+        self.messages_layout = MessagesLayout(self.height(), self.core)
+        self.login_layout = LoginLayout(self.core)
+
+        self.setLayout(self.login_layout.get())
 
     def read_handler(self):
         while True:
@@ -89,7 +138,7 @@ if __name__ == '__main__':
 
     widget = ChatWidget(config)
     widget.resize(1200, 900)
-    widget.msg_signal.connect(widget.update_messages)
+    widget.msg_signal.connect(widget.messages_layout.update_messages)
     widget.show()
 
     sys.exit([app.exec_(), widget.send_disconnect_msg()])
